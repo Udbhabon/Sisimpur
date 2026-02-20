8a4f3bd869c3bf727435a40b2eb45c09d0507c0b
Arpan Dey <sbxp1966@gmail.com>
2026-02-21 00:41:10 +0600

    Remove frontend-next package and related files

--- frontend-next/components/layout/Background.tsx
+++
@@ -1,9 +0,0 @@
-export default function Background() {
-  return (
-    <>
-      <div className="bg-blob bg-blob-1" />
-      <div className="bg-blob bg-blob-2" />
-      <div className="bg-blob bg-blob-3" />
-    </>
-  );
-}
--- frontend-next/components/ui/Toaster.tsx
+++
@@ -1,2 +0,0 @@
-"use client";
-export { ToastProvider as Toaster, useToast } from "./ToastProvider";
